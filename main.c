06ee49478dc6b1285adf6c44313cc030014bb80b
SheikhUbair <sheikhubair30@gmail.com>
2026-07-11 22:38:01 +0530

    Made left, Right paddle and Ball

--- main.c
+++ main.c
@@ -35,11 +35,62 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    SDL_Renderer *renderer = SDL_CreateRenderer(window ,-1, SDL_RENDERER_ACCELERATED);
+    // A rednerer is like a paintbrush which lets u draw things on your SDl window
+    //  This above is used to create a renderer on your window and -1 bascailly pickd the best driver for
+    // renderering and renderer accelerated means that use hardware acceleration (GPU) if needed
+
+    if(renderer==NULL)
+    {
+        printf("Renderer Error : %s\n",SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
     int running = 1;// later running vlaue can be changed to 0 to stop the gameplay loop.
     SDL_Event event;// anyhting that will occur like an input it will be registered as an event.
 
     while(running)// to loop the game.
     {
+        SDL_SetRenderDrawColor(renderer,0,0,0,255);
+        // here these numbers are for RGB and 255 is aplha which means transparency
+        // if alpha is 255 the the color is visible if its 0 then the colour is not visible.
+        SDL_RenderClear(renderer);
+
+        SDL_Rect Left_paddle =
+        {
+            50,
+            200,
+            20,
+            125
+        };
+        SDL_RenderFillRect(renderer,&Left_paddle);//SDL draw a filled rectangle
+
+        SDL_Rect Right_paddle=
+        {
+            725,
+            200,
+            20,
+            125
+        };
+        SDL_RenderFillRect(renderer,&Right_paddle);
+
+        SDL_Rect ball=
+        {
+            350,
+            250,
+            15,
+            15
+        };
+        SDL_SetRenderDrawColor(renderer,255,255,255,255);
+        SDL_RenderFillRect(renderer,&Left_paddle);
+        SDL_RenderFillRect(renderer,&Right_paddle);
+        SDL_RenderFillRect(renderer,&ball);
+        SDL_RenderPresent(renderer);// draws everthing and displays it .
+
+
+
         while(SDL_PollEvent(&event))// Poll event is like scanf waiting for the player to do something.
         // and if the player does anyhting like press W or quit the game
         // SDL stores that info in event and then we pass that vale of event here.
@@ -50,6 +101,7 @@ int main(int argc, char *argv[])
             }
         }
     }
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
